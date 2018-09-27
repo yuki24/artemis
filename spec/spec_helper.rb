1d8012b4d1896c46b317bc0af9bf58e8bfe91d6a
Yuki Nishijima <yk.nishijima@gmail.com>
2018-09-27 19:41:14 -0400

    Switch to rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,27 @@
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.warnings = true
+  config.order = :random
+
+  if config.files_to_run.one?
+    config.default_formatter = "doc"
+  end
+
+  # Print the 10 slowest examples and example groups at the
+  # end of the spec run, to help surface which specs are running
+  # particularly slow.
+  # config.profile_examples = 10
+
+  # Seed global randomization in this process using the `--seed` CLI option.
+  # Setting this allows you to use `--seed` to deterministically reproduce
+  # test failures related to randomization by passing the same `--seed` value
+  # as the one that triggered the failure.
+  # Kernel.srand config.seed
+end
---
+++ spec/unit/client_spec.rb
@@ -0,0 +1,5 @@
+describe "example" do
+  it "fails!" do
+    expect(true).to eq(false)
+  end
+end
