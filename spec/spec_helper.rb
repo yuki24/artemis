67a1fcdb0990e1c9d01f8016673cc0ab2f34adf2
Yuki Nishijima <yk.nishijima@gmail.com>
2018-09-27 23:25:49 -0400

    Always require pry-byebug for now

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,6 @@
+require 'pry'
+require 'pry-byebug' if RUBY_ENGINE == 'ruby'
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
