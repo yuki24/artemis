e093bc81e64e41f92355049f1c56ee991117c00d
Yuki Nishijima <yk.nishijima@gmail.com>
2018-10-07 22:44:09 -0400

    Fix wrong spec description

--- spec/unit/adapters_spec.rb
+++ spec/unit/adapters_spec.rb
@@ -57,7 +57,7 @@ describe 'Adapters' do
     it_behaves_like 'an adapter'
   end
 
-  describe Artemis::Adapters::NetHttpAdapter do
+  describe Artemis::Adapters::NetHttpPersistentAdapter do
     let(:adapter) { Artemis::Adapters::NetHttpPersistentAdapter.new('http://localhost:8000', service_name: nil, timeout: 5, pool_size: 5) }
 
     it_behaves_like 'an adapter'
