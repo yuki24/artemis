167dbfd41dba5147d0122bae3ad61997c1bd30c8
Yuki Nishijima <yk.nishijima@gmail.com>
2018-10-13 18:09:50 -0400

    Add mentino to default_context in client template

--- lib/generators/artemis/templates/client.rb
+++ lib/generators/artemis/templates/client.rb
@@ -1,4 +1,10 @@
 <% module_namespacing do -%>
 class <%= class_name %> < Artemis::Client
+  # If an access token needs to be assigned to every request:
+  # self.default_context = {
+  #   headers: {
+  #     Authorization: "token ..."
+  #   }
+  # }
 end
 <% end %>
